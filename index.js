6a9d93b0accbb92b69be2e75ef63635eb55e8aad
Victor Cazanave <victor.cazanave@gmail.com>
2020-02-03 21:44:44 +0800

    Handle default options values with commander

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { generateFilesFromSpreadsheet } = require('./lib')
 // Used to get version number from package.json
 require('pkginfo')(module, 'version')
 
-// Default values used in documentation and script
+// Default options values
 const DEFAULT_BEAUTIFY = 0
 const DEFAULT_CLIENT = './client_secret.json'
 const DEFAULT_FORMAT = 'json'
@@ -25,32 +25,39 @@ program
 	.version(module.exports.version, '-v, --version')
 	.option(
 		'-b, --beautify <number>', // TODO: Allow string to handle tabs?
-		`number of spaces to insert white space in JSON/JS files (min: 0, max: 10, default: 0)`,
+		'number of spaces to insert white space in JSON/JS files (min: 0, max: 10)',
+		DEFAULT_BEAUTIFY,
 		parseInt,
 	)
 	.option(
 		'-c, --client <path>',
-		`path of client secret file (default: ${DEFAULT_CLIENT})`,
+		'path of client secret file',
+		DEFAULT_CLIENT,
 	)
 	.option(
 		'-f, --format <format>',
-		`format of generated files (available values are cjs, esm and json, default: ${DEFAULT_FORMAT})`,
+		'format of generated files (available values: "cjs", "esm", "json")',
+		DEFAULT_FORMAT,
 	)
 	.option(
 		'-k, --key <index>',
-		`index of key column (default: ${DEFAULT_KEY_INDEX})`,
+		'index of key column',
+		DEFAULT_KEY_INDEX,
 	)
 	.option(
 		'-l, --lang <index>',
-		`index of first language column (default: ${DEFAULT_LANG_INDEX})`,
+		'index of first language column',
+		DEFAULT_LANG_INDEX,
 	)
 	.option(
 		'-o, --output <path>',
-		`path of output directory (default: ${DEFAULT_OUTPUT_DIR})`,
+		'path of output directory',
+		DEFAULT_OUTPUT_DIR,
 	)
 	.option(
 		'-r, --range <range>',
-		`range of data to parse (default: ${DEFAULT_RANGE})`,
+		'range of data to parse',
+		DEFAULT_RANGE,
 	)
 	.option(
 		'-s, --spreadsheet <id>',
@@ -58,19 +65,20 @@ program
 	)
 	.option(
 		'-t, --token <path>',
-		`path of credentials file (default: ${DEFAULT_TOKEN})`,
+		'path of credentials file',
+		DEFAULT_TOKEN,
 	)
 	.parse(process.argv)
 
 // Run script
 generateFilesFromSpreadsheet(
-	program.client || DEFAULT_CLIENT,
-	program.token || DEFAULT_TOKEN,
+	program.client,
+	program.token,
 	program.spreadsheet,
-	program.range || DEFAULT_RANGE,
-	program.key || DEFAULT_KEY_INDEX,
-	program.lang || DEFAULT_LANG_INDEX,
-	program.output || DEFAULT_OUTPUT_DIR,
-	program.format || DEFAULT_FORMAT,
-	program.beautify || DEFAULT_BEAUTIFY,
+	program.range,
+	program.key,
+	program.lang,
+	program.output,
+	program.format,
+	program.beautify,
 )
